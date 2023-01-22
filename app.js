ca09f7d9fdb7914618b8e237d8066d5f02b9b386
JY6117125 <jiayi.li.cohort229@gmail.com>
2023-01-22 10:00:54 -0800

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getrandomnum(min,max),
     guessesLeft = 3;
 
 const game = document.querySelector('#game'),
@@ -13,21 +13,58 @@ const game = document.querySelector('#game'),
 minNum.textContent=min;
 maxNum.textContent=max;
 
-guessBtn.addEventListener('click', function(){
+game.addEventListener('click', function(e){
+    if(e.target.className==="Again"){
+        window.location.reload();
+    }
+});
+
+guessBtn.addEventListener('mousedown', function(){
     let guess = parseInt(guessInput.value);
     if(isNaN(guess)||guess<min||guess>max){
         setMessage('pls enter a number between '+min+' and ' +max ,'red');
     }
     if (guess===winningNum) 
     { 
-        guessInput.disabled=true;
-        guessInput.style.borderColor ='green';
-        setMessage(winningNum+" is correct, you win",'green')
+        gameover(true,winningNum+" is correct, you win");
+        // guessInput.disabled=true;
+        // guessInput.style.borderColor ='green';
+        // setMessage(winningNum+" is correct, you win",'green')
     }
     else{
+        guessesLeft -= 1;
+        if(guessesLeft === 0 ){
+            gameover(false,"you lost, right number was "+winningNum)
+            // guessInput.disabled=true;
+            // guessInput.style.borderColor ='red';
+            // setMessage("you lost, right number was "+winningNum,'red')
+        }
+        
+        else{
+            guessInput.style.borderColor ='red';
+            guessInput.value=''
+            setMessage(guess+' is not right, and you have '+guessesLeft+' guesses Left','red')
 
+        }
     }
+
 });
+
+function gameover(won, msg){
+    let color;
+    won===true?color="green":color="red";
+    guessInput.disabled=true;
+    guessInput.style.borderColor = color;
+    message.style.color=color;
+    setMessage(msg)
+
+    guessBtn.value='Again';
+    guessBtn.className+='Again'
+}
+
+function getrandomnum(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
 
 function setMessage(msg, color){
     message.style.color= color;
